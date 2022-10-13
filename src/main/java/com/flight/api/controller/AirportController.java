3ff9cc02f50603e00bc9add42cc68bb1375f4389
Rafal Wisniewski <2krafal.wisniewski@gmail.com>
2022-10-13 21:42:19 +0200

    add POST /airport/raw

--- src/main/java/com/flight/api/controller/AirportController.java
+++ src/main/java/com/flight/api/controller/AirportController.java
@@ -3,6 +3,8 @@ package com.flight.api.controller;
 import com.flight.api.model.dto.AirportDTO;
 import com.flight.api.model.dto.raw.AirportRAW;
 import com.flight.api.service.AirportService;
+import org.apache.catalina.mapper.Mapper;
+import org.modelmapper.ModelMapper;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +16,7 @@ import java.util.List;
 @RequestMapping("/airport")
 public class AirportController {
     private final AirportService airportService;
+    private ModelMapper modelMapper = new ModelMapper();
 
     public AirportController(AirportService airportService) {
         this.airportService = airportService;
@@ -22,6 +25,11 @@ public class AirportController {
     @GetMapping("/{code}")
     public AirportDTO getAirport(@PathVariable String code){
         return airportService.findByCode(code);
+    }
+
+    @GetMapping("/raw/{code}")
+    public AirportRAW getAirportRaw(@PathVariable String code){
+        return modelMapper.map(airportService.findByCode(code), AirportRAW.class);
     }
 
     @GetMapping("/airports")
